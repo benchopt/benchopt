7ea1733624728c119f06b88c4f39642f17ae2c9a
tomMoral <thomas.moreau.2010@gmail.com>
2020-03-18 18:57:22 +0100

    TST add flag to skip install

--- conftest.py
+++ conftest.py
@@ -1,3 +1,29 @@
+import pytest
+
 from benchopt.config import DEFAULT_GLOBAL
+
+
 DEFAULT_GLOBAL['debug'] = True
 DEFAULT_GLOBAL['print_install_error'] = True
+
+
+def pytest_addoption(parser):
+    parser.addoption("--skip-install", action="store_true",
+                     help="skip install of solvers that can slow down CI.")
+
+
+def pytest_configure(config):
+    """Setup pytest for benchopt testing"""
+
+    config.addinivalue_line("markers", "requires_install")
+
+
+def pytest_collection_modifyitems(config, items):
+    if not config.getoption("--skip-install"):
+        return
+
+    skip_install = pytest.mark.skip(
+            reason="--skip-install option provided")
+    for item in items:
+        if "requires_install" in item.keywords:
+            item.add_marker(skip_install)
