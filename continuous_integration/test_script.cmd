1905f24d2d6a19b7a6ef6eef700221238ac7ff37
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-03-18 12:03:57 +0100

    enable codecov

--- continuous_integration/test_script.cmd
+++ continuous_integration/test_script.cmd
@@ -1,5 +1,6 @@
-set DEFAULT_PYTEST_ARGS=-vlrx --timeout=60
+set DEFAULT_PYTEST_ARGS=-vlrx --timeout=60 --cov=benchopt
 
 call activate %VIRTUALENV%
 
 pytest --junitxml=%JUNITXML% %DEFAULT_PYTEST_ARGS%
+codecov
