9ad2b428936858c88102ffc3c2460cd1bf8f6963
mathurinm <mathurin.massias@gmail.com>
2020-03-20 17:07:17 +0100

    run on single lambda, set very low tolerance

--- benchmarks/lasso/solvers/gapsafe.py
+++ benchmarks/lasso/solvers/gapsafe.py
@@ -19,15 +19,10 @@ class Solver(BaseSolver):
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
-        self.lmbd_max = np.max(np.abs(X.T @ y))
 
     def run(self, n_iter):
-        # TODO determine optimal strategy to solve single lambda.
-        # Right now, take a geom grid of 100 lambdas from lmb_max
-        # to lmbd
-        lmbds = self.lmbd_max * np.geomspace(1, self.lmbd / self.lmbd_max,
-                                             num=100)
-        coefs_ = lasso_path(self.X, self.y, lmbds, max_iter=n_iter)
+        coefs_ = lasso_path(
+            self.X, self.y, [self.lmbd], max_iter=n_iter, eps=1e-14)
         self.coef_ = coefs_[:, -1]
 
     def get_result(self):
