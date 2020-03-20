648445d82295cbae194a7e251dde0b61c63c958b
mathurinm <mathurin.massias@gmail.com>
2020-03-20 15:26:31 +0100

    move L in run() + 1 inplace

--- benchmarks/lasso/solvers/baseline.py
+++ benchmarks/lasso/solvers/baseline.py
@@ -13,9 +13,9 @@ class Solver(BaseSolver):
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
 
-        self.L = np.linalg.norm(self.X, ord=2) ** 2
-
     def run(self, n_iter):
+        L = np.linalg.norm(self.X, ord=2) ** 2
+
         n_features = self.X.shape[1]
         w = np.zeros(n_features)
         t_new = 1
@@ -25,10 +25,10 @@ class Solver(BaseSolver):
                 t_new = (1 + np.sqrt(1 + 4 * t_old ** 2)) / 2
                 w_old = w.copy()
             grad = self.X.T.dot(self.X.dot(w) - self.y)
-            w -= 1 / self.L * grad
-            w = self.st(w, 1 / self.L * self.lmbd)
+            w -= grad / L
+            w = self.st(w, self.lmbd / L)
             if self.use_acceleration:
-                w = w + (t_old - 1.) / t_new * (w - w_old)
+                w += (t_old - 1.) / t_new * (w - w_old)
         self.w = w
 
     def st(self, w, mu):
