bc0f760ad2af64a560c9e791fd2a172817ec302d
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-03-25 09:57:58 +0100

    iter on message

--- benchmarks/nnls/solvers/nnls_scipy.py
+++ benchmarks/nnls/solvers/nnls_scipy.py
@@ -16,9 +16,13 @@ class Solver(BaseSolver):
         self.X, self.y = X, y
 
     def run(self, n_iter):
-        # n_iter is not used here:
-        # optimize.nnls(self.X, self.y, maxiter=n_iter) could produce
-        # a RuntimeError for some datasets like Boston.
+        # Passing n_iter as in:
+        # optimize.nnls(self.X, self.y, maxiter=n_iter)
+        # is not done here as it has apparently no impact
+        # on convergence and is just used to raise a RuntimeError
+        # if the number of iterations actually run exceeds n_iter.
+        # This avoids producing errors on various datasets such
+        # as Boston.
         self.w = optimize.nnls(self.X, self.y)[0]
 
     def get_result(self):
