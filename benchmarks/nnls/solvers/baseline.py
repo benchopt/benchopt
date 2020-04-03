2a31ea97f50a1ac4ba8b0e76c169e3c29e16e55d
josephsalmon <joseph.salmon.taff@gmail.com>
2020-04-03 12:26:56 +0200

    fixing ista

--- benchmarks/nnls/solvers/baseline.py
+++ benchmarks/nnls/solvers/baseline.py
@@ -22,16 +22,15 @@ class Solver(BaseSolver):
         w = np.zeros(n_features)
         t_new = 1
         for i in range(n_iter):
-            if self.use_acceleration:
-                t_old = t_new
-                t_new = (1 + np.sqrt(1 + 4 * t_old ** 2)) / 2
-                w_old = w.copy()
             grad = self.X.T.dot(self.X.dot(w) - self.y)
             w -= grad / L
             w = np.maximum(w, 0)
             if i >= (n_iter - 1):
                 self.w = w.copy()
             if self.use_acceleration:
+                t_old = t_new
+                t_new = (1 + np.sqrt(1 + 4 * t_old ** 2)) / 2
+                w_old = w.copy()
                 w += (t_old - 1.) / t_new * (w - w_old)
 
     def get_result(self):
