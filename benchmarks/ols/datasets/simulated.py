d40301c26e8d75fff59abe10fb4bed6418f3a0f5
josephsalmon <joseph.salmon.taff@gmail.com>
2020-05-01 15:30:57 +0200

    ols solver

---
+++ benchmarks/ols/datasets/simulated.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from benchopt.base import BaseDataset
+
+
+class Dataset(BaseDataset):
+
+    name = "Simulated"
+
+    # List of parameters to generate the datasets. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    parameters = {
+        'n_samples, n_features': [
+            (500, 1000),
+            (200, 5000)]
+    }
+
+    def __init__(self, n_samples=10, n_features=50, random_state=27):
+        # Store the parameters of the dataset
+        self.n_samples = n_samples
+        self.n_features = n_features
+        self.random_state = random_state
+
+        # Pass parameters that will be used in Dataset name
+        super().__init__(n_samples=n_samples, n_features=n_features)
+
+    def get_data(self):
+
+        rng = np.random.RandomState(self.random_state)
+        X = rng.randn(self.n_samples, self.n_features)
+        y = rng.randn(self.n_samples)
+
+        data = dict(X=X, y=y)
+
+        return self.n_features, data
---
+++ benchmarks/ols/objective.py
@@ -0,0 +1,24 @@
+from benchopt.base import BaseObjective
+
+
+class Objective(BaseObjective):
+    name = "Ordinarly Least Squares"
+
+    parameters = {
+        'fit_intercept': [False],
+    }
+
+    def __init__(self, fit_intercept=False):
+        self.fit_intercept = fit_intercept
+
+        super().__init__(fit_intercept=fit_intercept)
+
+    def set_data(self, X, y):
+        self.X, self.y = X, y
+
+    def __call__(self, beta):
+        diff = self.y - self.X.dot(beta)
+        return .5 * diff.dot(diff)
+
+    def to_dict(self):
+        return dict(X=self.X, y=self.y, fit_intercept=self.fit_intercept)
