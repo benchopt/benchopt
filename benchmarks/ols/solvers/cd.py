41eea3f99e83202df42a331245ebf0ca7d7dd8bb
josephsalmon <joseph.salmon.taff@gmail.com>
2020-05-01 17:16:46 +0200

    solving Lj=0 issue in cd

--- benchmarks/ols/solvers/cd.py
+++ benchmarks/ols/solvers/cd.py
@@ -52,6 +52,8 @@ class Solver(BaseSolver):
         w = np.zeros(n_features)
         for _ in range(n_iter):
             for j in range(n_features):
+                if L[j] == 0.:
+                    continue
                 old = w[j]
                 w[j] = w[j] + X[:, j] @ R / L[j]
                 diff = old - w[j]
@@ -67,6 +69,8 @@ class Solver(BaseSolver):
         R = np.copy(y)
         for _ in range(n_iter):
             for j in range(n_features):
+                if L[j] == 0.:
+                    continue
                 old = w[j]
                 start, end = X_indptr[j:j+2]
                 scal = 0.
