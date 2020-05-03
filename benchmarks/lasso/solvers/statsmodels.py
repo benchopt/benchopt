004c405ac05d1f212b5777fc827e662b01c11335
josephsalmon <joseph.salmon.taff@gmail.com>
2020-05-04 00:06:40 +0200

    adding statsmodels solver for lasso

---
+++ benchmarks/lasso/solvers/statsmodels.py
@@ -0,0 +1,30 @@
+from benchopt.base import BaseSolver
+from benchopt.util import safe_import
+
+
+with safe_import() as solver_import:
+    import statsmodels.api as sm
+
+
+class Solver(BaseSolver):
+    name = 'statsmodels'
+
+    install_cmd = 'pip'
+    requirements = ['statsmodels']
+    requirements_import = ['statsmodels']
+
+    def set_objective(self, X, y, lmbd):
+        self.X, self.y, self.lmbd = X, y, lmbd
+        self.n_samples = self.X.shape[0]
+        self.clf = sm.GLS(y, X, hasconst=False)
+
+    def run(self, n_iter):
+
+        self.results = self.clf.fit_regularized(alpha=self.lmbd / self.n_samples,
+                                                method='elastic_net',
+                                                L1_wt=1, maxiter=n_iter,
+                                                cnvrg_tol=1e-14,
+                                                zero_tol=1e-14)
+
+    def get_result(self):
+        return self.results.params.flatten()
