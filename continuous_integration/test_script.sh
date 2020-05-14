1b45b1fa61ae3c302f149ce70acd8a056ea9990d
mathurinm <mathurin.massias@gmail.com>
2020-05-14 10:41:36 +0200

    also use . activate in test_script.sh

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -3,8 +3,8 @@
 set -e
 
 # if [[ "$PACKAGER" == "conda" ]]; then
-. $CONDA_PREFIX/etc/profile.d/conda.sh
-conda activate $CONDAENV
+# . $CONDA_PREFIX/etc/profile.d/conda.sh
+. activate $CONDAENV
 # elif [[ "$PACKAGER" == "ubuntu" ]]; then
     # source $CONDAENV/bin/activate
 # fi
