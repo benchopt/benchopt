4d1c7e8c7cbfb6c7ee1c7cf9fda31580255854d6
tomMoral <thomas.moreau.2010@gmail.com>
2020-07-10 15:11:42 +0200

    FIX make sure the test env contains conda-forge channel

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -9,7 +9,8 @@ TO_INSTALL="python=$VERSION_PYTHON pip $TO_INSTALL"
 
 conda create -n $CONDAENV --yes $TO_INSTALL
 . activate $CONDAENV
-# conda config --append channels conda-forge
+conda config --env --prepend channels conda-forge
+
 echo $CONDA
 
 python --version
