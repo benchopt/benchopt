d21b1dceb28d3126aacf823f5d68015c4b10920a
tomMoral <thomas.moreau.2010@gmail.com>
2020-09-01 17:01:07 +0200

    CLN hide julia Pycall compilation output

--- benchopt/utils/julia.py
+++ benchopt/utils/julia.py
@@ -1,5 +1,7 @@
 import julia
 
+from .stream_redirection import SuppressStd
+
 
 # Factory to get the julia interpreter only once
 jl_interpreter = None
@@ -8,9 +10,15 @@ jl_interpreter = None
 def get_jl_interpreter():
     global jl_interpreter
     if jl_interpreter is None:
-        # configure the julia runtime
-        runtime_config = {'compiled_modules': False}
-        julia.install()
-        jl_interpreter = julia.Julia(**runtime_config)
+        out = SuppressStd()
+        try:
+            with out:
+                # configure the julia runtime
+                runtime_config = {'compiled_modules': False}
+                julia.install()
+                jl_interpreter = julia.Julia(**runtime_config)
+        except BaseException:
+            print(out.output)
+            raise
 
     return jl_interpreter
