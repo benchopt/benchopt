af7d599f29c002edb1f9bbd724fe452f3dc46f3a
tomMoral <thomas.moreau.2010@gmail.com>
2020-09-08 11:16:48 +0200

    CLN remove ouputs

--- examples/plot_run_benchmark_python_R_julia.py
+++ examples/plot_run_benchmark_python_R_julia.py
@@ -21,7 +21,7 @@ df = run_benchmark('benchmarks/lasso',
                    dataset_names=['Simulated*n_samples=100,n_features=500*'],
                    objective_filters=['reg=0.5'],
                    max_runs=100, timeout=100, n_repetitions=5,
-                   plot_result=False)
+                   plot_result=False, show_progress=False)
 
 figs = plot_benchmark(df, benchmark='benchmarks/lasso')
 
