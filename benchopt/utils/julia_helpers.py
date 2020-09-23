fca47417c1faebbe62ae3552259d4af64284d7d7
Thomas Moreau <thomas.moreau.2010@gmail.com>
2020-09-23 11:38:55 +0200

    FIX skip julia test in OSX due to ongoing segfault (#65)

--- benchopt/utils/julia_helpers.py
+++ benchopt/utils/julia_helpers.py
@@ -1,24 +1,37 @@
 import julia
+from contextlib import contextmanager
 
+from ..config import DEBUG
 from .stream_redirection import SuppressStd
 
+# nullcontext is not available in python <=3.6 so we resort to this
+# for backward compat.
+@contextmanager
+def nullcontext(enter_result=None):
+    yield enter_result
 
-# Factory to get the julia interpreter only once
+
+# Singleton to get the julia interpreter only once
 jl_interpreter = None
 
 
 def get_jl_interpreter():
     global jl_interpreter
     if jl_interpreter is None:
-        out = SuppressStd()
+        # Only suppress std if not in debug mode.
+        out = nullcontext() if DEBUG else SuppressStd()
         try:
             with out:
                 # configure the julia runtime
-                runtime_config = {'compiled_modules': False}
+                runtime_config = {
+                    'compiled_modules': False,
+                    'debug': bool(DEBUG)
+                }
                 julia.install()
                 jl_interpreter = julia.Julia(**runtime_config)
         except BaseException:
-            print(out.output)
+            if hasattr(out, 'output'):
+                print(out.output)
             raise
 
     return jl_interpreter
