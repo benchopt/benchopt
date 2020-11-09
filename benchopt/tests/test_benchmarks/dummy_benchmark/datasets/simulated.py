3a078599de6765e766fb250125aea1f78a68740c
tomMoral <thomas.moreau.2010@gmail.com>
2020-11-09 19:41:02 +0100

    CLN adress @mathurinm comments

--- benchopt/tests/test_benchmarks/dummy_benchmark/datasets/simulated.py
+++ benchopt/tests/test_benchmarks/dummy_benchmark/datasets/simulated.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from benchopt.base import BaseDataset
-from benchopt.utils.datasets.simulated import gen_correlated_data
+from benchopt.utils.datasets.simulated import make_correlated_data
 
 
 class Dataset(BaseDataset):
@@ -13,8 +13,8 @@ class Dataset(BaseDataset):
     parameters = {
         'n_samples, n_features': [
             (100, 5000),
-            (100, 10000)],  # slow to simulate big correlated design
-        'rho': [0, 0.6]
+            (100, 10000)
+        ], 'rho': [0, 0.6]
     }
 
     def __init__(self, n_samples=10, n_features=50, random_state=27, rho=0):
@@ -29,8 +29,8 @@ class Dataset(BaseDataset):
         if self.rho == 0:
             X = rng.randn(self.n_samples, self.n_features)
         else:
-            X = gen_correlated_data(self.n_samples, self.n_features,
-                                    rho=self.rho, random_state=rng)
+            X = make_correlated_data(self.n_samples, self.n_features,
+                                     rho=self.rho, random_state=rng)
 
         y = rng.randn(self.n_samples)
 
--- benchopt/utils/datasets/simulated.py
+++ benchopt/utils/datasets/simulated.py
@@ -1,24 +1,23 @@
 """Helper to generate simulated data in benchopt."""
 import numpy as np
+from sklearn.utils import check_random_state
 
-from ..checkers import check_random_state
 
-
-def gen_correlated_data(n_samples=100, n_features=50, rho=0.6,
-                        random_state=None):
-    """Generate correlated design matrix with decaying correlation rho**|i-j|.
+def make_correlated_data(n_samples=100, n_features=50, rho=0.6,
+                         random_state=None):
+    r"""Generate correlated design matrix with decaying correlation rho**|i-j|.
 
     The data are generated using an AR model with reason corr and innovation
-    $\\sigma^2 = 1 - \\rho^2$,
+    $\sigma^2 = 1 - \rho^2$,
     $$
-        x_{i+1} = \\rho x_i + \\epsilon \\quad
-        where \\quad \\epsilon \\sim \\mathcal N(0, \\sigma^2)
+        x_{i+1} = \rho x_i + \epsilon \quad
+        where \quad \epsilon \sim \mathcal N(0, \sigma^2)
     $$
     The generated features have mean 0, variance 1 and the expected correlation
     structure
     $$
-        \\mathbb E[x_i] = 0~, \\quad \\mathbb E[x_i^2] = 1  \\quad
-        and \\quad \\mathbb E[x_ix_j] = \\rho^{|i-j|}
+        \mathbb E[x_i] = 0~, \quad \mathbb E[x_i^2] = 1  \quad
+        and \quad \mathbb E[x_ix_j] = \rho^{|i-j|}
     $$
 
     Parameters
@@ -28,19 +27,19 @@ def gen_correlated_data(n_samples=100, n_features=50, rho=0.6,
     n_features: int
         Number of features in the design matrix.
     corr: float
-        Correlation $\\rho$ between successive features. The element $C_{i, j}$
-        in the correlation matrix will be $\\rho^{|i-j|}$. This parameter
+        Correlation $\rho$ between successive features. The element $C_{i, j}$
+        in the correlation matrix will be $\rho^{|i-j|}$. This parameter
         should be selected in $[0, 1[$.
     random_state : int | RandomState instance | None (default)
-        Determines random number generation for centroid initialization and
-        random reassignment. Use an int to make the randomness deterministic.
+        Determines random number generation for data generation. Use an int to
+        make the randomness deterministic.
 
     Returns
     -------
     X: ndarray, shape (n_samples, n_features)
         A design matrix with Toeplitz covariance.
     """
-    if rho >= 1 or rho < 0:
+    if not 0 <= rho < 1:
         raise ValueError("The correlation `rho` should be chosen in [0, 1[.")
     rng = check_random_state(random_state)
 
@@ -48,10 +47,10 @@ def gen_correlated_data(n_samples=100, n_features=50, rho=0.6,
     innovation = rng.randn(n_features + 1, n_samples)
     U = innovation[0]
 
-    res = [U.copy()]
+    X = np.empty([n_samples, n_features])
+    X[:, 0] = U
     for i in range(1, n_features):
         U *= rho
         U += sigma * innovation[i]
-        res.append(U.copy())
-    X = np.array(res)
+        X[:, i] = U
     return X.T
