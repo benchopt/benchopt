07a4b99d6f3132c1cbf2c7497581d8cc90b50602
Tanguy Lefort <tanguy_l@outlook.fr>
2020-12-10 18:32:49 +0100

    Fix PGD with R (LAPACK import)

--- benchopt/utils/r_helpers.py
+++ benchopt/utils/r_helpers.py
@@ -1,27 +1,30 @@
 import os
 
-# Make sure that R_HOME is loaded from the current interpreter to avoid
-# using the parent interpreter R_HOME in the sub-interpreter.
+# # Make sure that R_HOME is loaded from the current interpreter to avoid
+# # using the parent interpreter R_HOME in the sub-interpreter.
 if os.environ.get('R_HOME', None) is not None:
-    del os.environ['R_HOME']
+    os.environ.pop('R_HOME')
 
 
 import rpy2  # noqa: E402
 import rpy2.robjects.packages as rpackages  # noqa: E402
 try:
     from rpy2.robjects.packages import PackageNotInstalledError
+    import rpy2.situation
 except ImportError:
     # Backward compat for rpy2 version < 3.3
     try:
         from rpy2.rinterface_lib.embedded import \
             RRuntimeError as PackageNotInstalledError
+        import rpy2.situation
     except ImportError:
         # Backward compat for rpy2 version < 3
         from rpy2.rinterface import RRuntimeError as PackageNotInstalledError
 
 # Hide the R warnings
 rpy2.robjects.r['options'](warn=-1)
-
+# Set the R_HOME directory to the one of the R RHOME ouput
+os.environ['R_HOME'] = rpy2.situation.r_home_from_subprocess()
 
 def import_rpackages(*packages):
     """Helper to import R packages in the import_ctx"""
