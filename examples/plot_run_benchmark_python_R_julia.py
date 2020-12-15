1d211700e85a42e7e01bc3d1148532081ae2553f
tomMoral <thomas.moreau.2010@gmail.com>
2020-12-15 09:41:19 +0100

    DOC restrain example for R/Julia to one run

--- examples/plot_run_benchmark_python_R_julia.py
+++ examples/plot_run_benchmark_python_R_julia.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from benchopt import run_benchmark
 from benchopt.benchmark import Benchmark
+from benchopt.tests import SELECT_ONE_SIMULATED
 from benchopt.viz import plot_benchmark, PLOT_KINDS
 
 
@@ -19,7 +20,7 @@ try:
     df = run_benchmark(
         Benchmark(BENCHMARK_PATH),
         ['Python-PGD*use_acceleration=False', 'R-PGD', 'Julia-PGD'],
-        dataset_names=['Simulated*n_samples=100,n_features=500*'],
+        dataset_names=[SELECT_ONE_SIMULATED],
         objective_filters=['reg=0.5'],
         max_runs=100, timeout=100, n_repetitions=5,
         plot_result=False, show_progress=False
