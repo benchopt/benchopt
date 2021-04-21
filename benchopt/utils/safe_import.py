d2b9599d7af422818f1542452c9c6f248c08bd33
tomMoral <thomas.moreau.2010@gmail.com>
2021-04-21 19:25:54 +0200

    MTN auto versioning + move to setup.cfg

--- benchopt/utils/safe_import.py
+++ benchopt/utils/safe_import.py
@@ -19,6 +19,7 @@ class safe_import_context:
     """Do not fail on ImportError and catch import warnings"""
 
     def __init__(self):
+        self.skip_import = False
         self.failed_import = False
         self.record = warnings.catch_warnings(record=True)
 
@@ -42,6 +43,7 @@ class safe_import_context:
     def __exit__(self, exc_type, exc_value, tb):
 
         if SKIP_IMPORT:
+            self.skip_import = True
             self.failed_import = True
             return True
 
--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -4,8 +4,7 @@ set -e
 
 UNAMESTR=`uname`
 
-TO_INSTALL=$(cat dev-requirements.txt)
-TO_INSTALL="python=$VERSION_PYTHON pip $TO_INSTALL"
+TO_INSTALL="python=$VERSION_PYTHON pip"
 
 conda create -n $CONDAENV --yes $TO_INSTALL
 . activate $CONDAENV
@@ -17,4 +16,4 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "from joblib import cpu_count; print('%d CPUs' % cpu_count())"
 pip list
-pip install -e .
+pip install -e .[test]
