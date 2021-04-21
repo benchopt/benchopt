23e84fcf6010cef8c9ca5709a00cb66ee65e8307
tomMoral <thomas.moreau.2010@gmail.com>
2021-04-21 23:13:26 +0200

    iFIX lightning install for examples

--- examples/plot_run_benchmark.py
+++ examples/plot_run_benchmark.py
@@ -18,15 +18,15 @@ BENCHMARK_PATH = Path(os.getcwd()).parent / 'benchmarks' / 'logreg_l2'
 
 try:
     save_file = run_benchmark(
-        Benchmark(BENCHMARK_PATH), ['sklearn', 'lightning'],
+        Benchmark(BENCHMARK_PATH), ['sklearn', 'lightning', 'python-gd'],
         dataset_names=['Simulated*n_samples=200,n_features=500*'],
         max_runs=100, timeout=20, n_repetitions=3,
         plot_result=False, show_progress=False
     )
 except RuntimeError:
     raise RuntimeError(
-        "This example can only work when Lasso benchmark is cloned in the "
-        "example folder. Please run:\n"
+        "This example can only work when Logreg-l2 benchmark is cloned in a "
+        "`benchmarks` folder. Please run:\n"
         "$ git clone https://github.com/benchopt/benchmark_logreg_l2 "
         f"{BENCHMARK_PATH.resolve()}"
     )
