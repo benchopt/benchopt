688e71037d6378c453f262d343f43103f8ef0b26
tomMoral <thomas.moreau.2010@gmail.com>
2021-04-22 00:10:23 +0200

    FIx indentation in yml file

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -11,6 +11,7 @@ TEST_CMD="$TEST_CMD --test-env $CONDAENV --pyargs"
 # TEST_CMD="$TEST_CMD --skip-install"
 
 if [[ "$COVERAGE" == "true" ]]; then
+    export COVERAGE_PROCESS_START=".coveragerc"
     TEST_CMD="$TEST_CMD --cov=benchopt --cov-config=.coveragerc"
     python continuous_integration/install_coverage_subprocess_pth.py
 fi
