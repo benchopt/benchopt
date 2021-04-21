89f3605e348d8b38ec7c830927d85d3793987f1d
tomMoral <thomas.moreau.2010@gmail.com>
2021-04-21 19:34:27 +0200

    FIx add setup.cfg

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -13,7 +13,9 @@ conda config --env --prepend channels conda-forge
 echo $CONDA
 
 python --version
+pip install -e .[test]
+
+
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "from joblib import cpu_count; print('%d CPUs' % cpu_count())"
 pip list
-pip install -e .[test]
