ecaaa11a97cb11075f0e58a9772ff3143268b984
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-04-22 17:11:56 +0200

    some cleanup

--- benchopt/tests/test_benchmarks/dummy_benchmark/solvers/cd.py
+++ benchopt/tests/test_benchmarks/dummy_benchmark/solvers/cd.py
@@ -1,10 +1,9 @@
-import numpy as np
-from scipy import sparse
-
 from benchopt import BaseSolver
 from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    import numpy as np
+    from scipy import sparse
     from numba import njit
 
 if import_ctx.failed_import:
--- benchopt/tests/test_benchmarks/dummy_benchmark/solvers/sklearn.py
+++ benchopt/tests/test_benchmarks/dummy_benchmark/solvers/sklearn.py
@@ -12,7 +12,7 @@ class Solver(BaseSolver):
     name = 'sklearn'
 
     install_cmd = 'conda'
-    requirements = ['scikit-learn']
+    requirements = ['pip:scikit-learn']
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
