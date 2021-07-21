21d529bf087f231d75fd6a37f3292725c6b1044d
mathurinm <mathurin.massias@gmail.com>
2021-07-21 15:22:41 +0200

    fix patterns in tests

--- benchopt/tests/__init__.py
+++ benchopt/tests/__init__.py
@@ -9,9 +9,9 @@ TEST_BENCHMARK_DIR = Path(__file__).parent / 'test_benchmarks'
 DUMMY_BENCHMARK_PATH = TEST_BENCHMARK_DIR / 'dummy_benchmark'
 
 # Pattern to select specific datasets or solvers.
-SELECT_ONE_SIMULATED = r'simulated*500*rho=0\]'
-SELECT_ONE_PGD = r'python-pgd*step_size=1\]'
-SELECT_ONE_OBJECTIVE = r'dummy*reg=0.1\]'
+SELECT_ONE_SIMULATED = r'simulated*500*rho=0]'
+SELECT_ONE_PGD = r'python-pgd*step_size=1]'
+SELECT_ONE_OBJECTIVE = r'dummy*reg=0.1]'
 
 try:
     DUMMY_BENCHMARK = Benchmark(DUMMY_BENCHMARK_PATH)
