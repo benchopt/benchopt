2d043bef827bf8a4d83b73dd568f8f015b4d425b
Tanguy Lefort <tanguy_l@outlook.fr>
2021-09-16 21:11:53 +0200

    backticks

--- benchopt/plotting/plot_objective_curve.py
+++ benchopt/plotting/plot_objective_curve.py
@@ -44,8 +44,7 @@ def plot_objective_curve(df, obj_col='objective_value', plotly=False,
     dataset_name = df['data_name'].unique()[0]
     objective_name = df['objective_name'].unique()[0]
     title = f"{objective_name}\nData: {dataset_name}"
-
-    df.query(f"{obj_col} not in [inf, -inf]", inplace=True)
+    df.query(f"`{obj_col}` not in [inf, -inf]", inplace=True)
     y_label = "F(x)"
     if suboptimality:
         eps = 1e-10
