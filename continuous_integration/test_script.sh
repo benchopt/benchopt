169a19f79e6dba88f6e724a37ebc08f3d316235a
tomMoral <thomas.moreau.2010@gmail.com>
2021-09-28 13:58:35 +0200

    DBG only run sklearn+r-pgd

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -18,7 +18,8 @@ if [[ "$COVERAGE" == "true" ]]; then
 fi
 
 set -x
-$TEST_CMD benchopt/tests/test_benchmarks.py::test_solver_install[dummy_benchmark-sklearn]
+$TEST_CMD benchopt/tests/test_benchmarks.py::test_solver_install[dummy_benchmark-r-pgd] \
+          benchopt/tests/test_benchmarks.py::test_solver_install[dummy_benchmark-sklearn]
 $TEST_CMD --skip-install --cov-append
 set +x
 
