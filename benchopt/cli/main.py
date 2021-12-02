259f951ea119cbd4659cd80b0016757c1a71ff70
mathurinm <mathurinm@users.noreply.github.com>
2021-12-02 18:18:45 +0100

    ENH quiet flag in dependencies install (#254)

--- benchopt/cli/main.py
+++ benchopt/cli/main.py
@@ -276,7 +276,7 @@ def install(benchmark, solver_names, dataset_names, force=False,
     print("# Install", flush=True)
     benchmark.install_all_requirements(
         include_solvers=solver_names, include_datasets=dataset_names,
-        env_name=env_name, force=force,
+        env_name=env_name, force=force, quiet=quiet,
     )
 
 
