94264e6c2fc32fb30d479b05c1d4274055820fa4
mathurinm <mathurinm@users.noreply.github.com>
2022-04-14 08:15:08 +0200

    ENH indent CLI output (#320)
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- benchopt/runner.py
+++ benchopt/runner.py
@@ -399,7 +399,7 @@ def run_benchmark(benchmark, solver_names=None, forced_solvers=None,
                 objective = objective_class.get_instance(**obj_parameters)
                 if not is_matched(str(objective), objective_filters):
                     continue
-                print_normalize(f"|--{objective}")
+                print_normalize(f"  |--{objective}")
                 objective.set_dataset(dataset)
 
                 for solver_class in solver_classes:
@@ -414,7 +414,7 @@ def run_benchmark(benchmark, solver_names=None, forced_solvers=None,
                             continue
 
                         # Get the solver's name
-                        tag = colorify(f"|----{solver}:")
+                        tag = colorify(f"    |--{solver}:")
 
                         # check if the module caught a failed import
                         if not solver.is_installed(
@@ -431,7 +431,7 @@ def run_benchmark(benchmark, solver_names=None, forced_solvers=None,
                                 f"{tag} {colorify('skip', YELLOW)}"
                             )
                             if reason is not None:
-                                print(f'Reason: {reason}')
+                                print(f'    Reason: {reason}')
                             continue
 
                         # Get meta
