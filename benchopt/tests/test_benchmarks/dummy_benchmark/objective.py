4cfe7ab2abf7097a45c7c2bdfc738ccabbd3f662
Thomas Moreau <thomas.moreau.2010@gmail.com>
2022-04-26 11:16:48 +0200

    FIX skip for test objective

--- benchopt/tests/test_benchmarks/dummy_benchmark/objective.py
+++ benchopt/tests/test_benchmarks/dummy_benchmark/objective.py
@@ -21,7 +21,7 @@ class Objective(BaseObjective):
         self.lmbd = self.reg * self._get_lambda_max()
 
     def skip(self, X, y):
-        if np.all(self.X == 0):
+        if np.all(X == 0):
             return True, 'X is all zeros'
         return False, None
 
