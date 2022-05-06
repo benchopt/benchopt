76411e554315080b2d87bbffdb16a8ed1b4cc3c2
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-06 17:03:08 +0200

    osx

--- benchopt/tests/test_benchmarks/dummy_benchmark/test_config.py
+++ benchopt/tests/test_benchmarks/dummy_benchmark/test_config.py
@@ -12,6 +12,6 @@ def check_test_solver_install(solver_class):
     # conflict with conda packages for R
     # See issue #64
     # julia=1.6.4 broke on conda, see PR #252
-    if 'julia' in solver_class.name.lower():
+    if 'julia' in solver_class.name.lower() and sys.platform == 'darwin':
         # pytest.xfail('Julia causes segfault on OSX for now.')
         pytest.xfail('Julia install via conda is broken for now on macos.')
