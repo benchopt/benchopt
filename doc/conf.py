5949aa4da04d8ab180df365ff4e9303238e04266
bcharlier <benjamin.charlier@umontpellier.fr>
2022-05-13 09:23:49 +0200

    DOC add contribution guidelines  (#361)

--- doc/conf.py
+++ doc/conf.py
@@ -33,6 +33,7 @@ sys.path.append(os.path.abspath(os.path.join(curdir, 'sphinxext')))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx-prompt',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
