benchopt/benchopt

commit 6b7821141029c191026c8c81559ffe7dda03a680

ENH use current working directory as default in CLI (#384)
Co-authored-by: mathurinm <mathurin.massias@gmail.com>

benchopt/cli/process_results.py
@@ -1,4 +1,5 @@
 import click
+from pathlib import Path
 
 from benchopt.config import get_setting
 from benchopt.benchmark import Benchmark
@@ -20,7 +21,7 @@ def get_plot_kinds(ctx, args, incomplete):
 @process_results.command(
     help="Plot the result from a previously run benchmark."
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.option('--filename', '-f', type=str, default=None,
               shell_complete=complete_output_files,
@@ -70,7 +71,7 @@ def plot(benchmark, filename=None, kinds=('suboptimality_curve',),
     "See the :ref:`publish_doc` documentation for more info on how to use "
     "this command."
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.option('--token', '-t', type=str, default=None,
               help="Github token to access the result repo.")
@@ -105,7 +106,8 @@ def publish(benchmark, token=None, filename=None):
     help="Generate result website from list of benchmarks."
 )
 @click.option('--benchmark', '-b', 'benchmarks', metavar="<bench>",
-              multiple=True, type=click.Path(exists=True),
+              multiple=True, default=[Path.cwd()],
+              type=click.Path(exists=True),
               shell_complete=complete_benchmarks,
               help="Folders containing benchmarks to include.")
 @click.option('--pattern', '-k', 'patterns',

benchopt/cli/completion.py
@@ -46,6 +46,7 @@ def complete_benchmarks(ctx, param, incomplete):
 
 def find_benchmark_in_args(args):
     "Find the benchmark in preceeding args for benchmark dependent completion."
+    args.extend([Path.cwd()])  # default path is current working directory
     for b in args:
         if (Path(b) / "objective.py").exists():
             return Benchmark(b)

benchopt/cli/helpers.py
@@ -31,7 +31,7 @@ helpers = click.Group(
     help="Clean the cache and the outputs from a benchmark.",
     options_metavar=''
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 def clean(benchmark, token=None, filename=None):
 
@@ -185,7 +185,7 @@ def print_info(cls_name_list, cls_list, env_name=None, verbose=False):
     help="List information (solvers/datasets) and corresponding requirements "
     "for a given benchmark."
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.option('--solver', '-s', 'solver_names',
               metavar="<solver_name>", multiple=True, type=str,
@@ -378,7 +378,7 @@ def get(ctx, name):
     "class BASE_CLASS_NAME.",
     hidden=True
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.argument('module_filename', nargs=1, type=Path)
 @click.argument('base_class_name', nargs=1, type=str)

benchopt/cli/main.py
@@ -67,7 +67,7 @@ def _get_run_args(cli_kwargs, config_file_kwargs):
     "in a benchmark-dedicated conda environment or in your own "
     "conda environment, see the command `benchopt install`."
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.option('--objective-filter',
               metavar='<objective_filter>', multiple=True, type=str,
@@ -267,7 +267,7 @@ def run(config_file=None, **kwargs):
 @main.command(
     help="Install the requirements (solvers/datasets) for a benchmark."
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.option('--force', '-f',
               is_flag=True,
@@ -402,7 +402,7 @@ def install(
     "benchopt.github.io/how.html#example-of-parametrized-simulated-dataset",
     context_settings=dict(ignore_unknown_options=True)
 )
-@click.argument('benchmark', type=click.Path(exists=True),
+@click.argument('benchmark', default=Path.cwd(), type=click.Path(exists=True),
                 shell_complete=complete_benchmarks)
 @click.option('--env-name', type=str, default=None, metavar='NAME',
               shell_complete=complete_conda_envs,

benchopt/tests/test_cli.py
@@ -50,6 +50,7 @@ DATASET_COMPLETION_CASES = [
     ('simu', ['simulated']),
     ('lated', ['simulated']),
 ]
+CURRENT_DIR = Path.cwd()
 
 
 def _get_completion(cmd, args, incomplete):
@@ -105,11 +106,15 @@ class TestRunCmd:
 
     @pytest.mark.parametrize('invalid_benchmark, match', [
         ('invalid_benchmark', "Path 'invalid_benchmark' does not exist."),
-        ('.', "The folder '.' does not contain `objective.py`")],
-        ids=['invalid_path', 'no_objective'])
+        ('.', "The folder '.' does not contain `objective.py`"),
+        ("", rf"The folder '{CURRENT_DIR}' does not contain `objective.py`")],
+        ids=['invalid_path', 'no_objective', "no_objective in default"])
     def test_invalid_benchmark(self, invalid_benchmark, match):
         with pytest.raises(click.BadParameter, match=match):
-            run([invalid_benchmark], 'benchopt', standalone_mode=False)
+            if len(invalid_benchmark) > 0:
+                run([invalid_benchmark], 'benchopt', standalone_mode=False)
+            else:
+                run([], 'benchopt', standalone_mode=False)
 
     def test_invalid_dataset(self):
         with pytest.raises(click.BadParameter, match=r"invalid_dataset"):

commit a02982281586699b12f5b3898a2fd27e57b8f9bb

FIX generate_result broken from #384

benchopt/cli/process_results.py
@@ -111,7 +111,7 @@ def publish(benchmark, token=None, filename=None):
     help="Generate result website from list of benchmarks."
 )
 @click.option('--benchmark', '-b', 'benchmarks', metavar="<bench>",
-              multiple=True, default=[Path.cwd()],
+              multiple=True,
               type=click.Path(exists=True),
               shell_complete=complete_benchmarks,
               help="Folders containing benchmarks to include.")

benchopt/plotting/generate_html.py
@@ -469,14 +469,17 @@ def plot_benchmark_html_all(patterns=(), benchmarks=(), root=None,
             fnames, PLOT_KINDS.keys(), root_html, benchmark.name, copy=True
         )
         len_fnames.append(len(fnames))
-        rendered = render_benchmark(
-            results, benchmark.name, static_dir=static_dir
-        )
+        if len(results) > 0:
+            rendered = render_benchmark(
+                results, benchmark.name, static_dir=static_dir
+            )
 
-        benchmark_filename = (root_html / benchmark.name).with_suffix('.html')
-        print(f"Writing {benchmark.name} results to {benchmark_filename}")
-        with open(benchmark_filename, "w") as f:
-            f.write(rendered)
+            benchmark_filename = (
+                root_html / benchmark.name
+            ).with_suffix('.html')
+            print(f"Writing {benchmark.name} results to {benchmark_filename}")
+            with open(benchmark_filename, "w") as f:
+                f.write(rendered)
 
         htmls = render_all_results(
             results, benchmark.name, static_dir=static_dir