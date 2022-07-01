ae62c76905bee42904267af4eda2bc2a371721d7
Melvine Nargeot <melvine.nargeot@gmail.com>
2022-07-01 11:18:13 +0200

    Fix tests

--- benchopt/cli/completion.py
+++ benchopt/cli/completion.py
@@ -86,7 +86,7 @@ def complete_output_files(ctx, param, incomplete):
     # autocompletion with relative paths
     cwd = Path().resolve()
     candidates = [
-        p.resolve().relative_to(cwd) for p in output_folder.glob('*.csv') + output_folder.glob('*.parquet')
+        p.resolve().relative_to(cwd) for p in output_folder.glob("*.[csv][parquet]")
     ]
     return propose_from_list(candidates, incomplete)
 
