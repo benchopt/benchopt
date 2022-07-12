cf7f8243cdaa2d245dc070a645349117e1200121
Melvine Nargeot <melvine.nargeot@gmail.com>
2022-07-12 17:18:59 +0200

    Fix sklearn max_iter in dummy benchmark

--- benchopt/tests/test_benchmarks/dummy_benchmark/solvers/sklearn.py
+++ benchopt/tests/test_benchmarks/dummy_benchmark/solvers/sklearn.py
@@ -1,4 +1,5 @@
 import warnings
+import numpy as np
 from benchopt import BaseSolver
 from benchopt import safe_import_context
 
@@ -22,8 +23,11 @@ class Solver(BaseSolver):
         warnings.filterwarnings('ignore', category=ConvergenceWarning)
 
     def run(self, n_iter):
-        self.clf.max_iter = n_iter
-        self.clf.fit(self.X, self.y)
+        if n_iter == 0:
+            self.coef = np.zeros([self.X.shape[1] + self.fit_intercept])
+        else:
+            self.clf.max_iter = n_iter
+            self.clf.fit(self.X, self.y)
 
     def get_result(self):
         return self.clf.coef_.flatten()
