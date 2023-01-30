4fc5eb0ab418e08425bbef85409d3cb08ea5ab2b
mathurinm <mathurin.massias@gmail.com>
2023-01-30 10:45:59 +0100

    syntactic sugar for timeout

--- benchopt/cli/main.py
+++ benchopt/cli/main.py
@@ -1,6 +1,7 @@
 import yaml
 import click
 import warnings
+import pandas as pd
 from pathlib import Path
 
 from benchopt.benchmark import Benchmark
@@ -124,8 +125,10 @@ def _get_run_args(cli_kwargs, config_file_kwargs):
               help='Number of repetitions that are averaged to estimate the '
               'runtime.')
 @click.option('--timeout',
-              metavar="<int>", default=100, show_default=True, type=int,
-              help='Timeout a solver when run for more than <timeout> seconds')
+              default=100, show_default=True, type=str,
+              help='Stop a solver when run for more than <timeout> seconds.'
+              ' The syntax 10h or 10m can be used to denote 10 hours or '
+              'minutes respectively.')
 @click.option('--config', 'config_file', default=None,
               shell_complete=complete_config_files,
               help="YAML configuration file containing benchmark options.")
@@ -182,6 +185,13 @@ def run(config_file=None, **kwargs):
         plot, html, pdb, do_profile, env_name, output,
         deprecated_objective_filters, old_objective_filters
     ) = _get_run_args(kwargs, config)
+
+    try:
+        timeout = int(timeout)
+        timeout = f('{timeout}s')
+    except ValueError:
+        pass
+    timeout = pd.to_timedelta(timeout).total_seconds()
 
     if len(old_objective_filters):
         warnings.warn(
