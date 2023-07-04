079fbb453288b378f2ff4cb10694741b0d4b2c53
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-07-04 11:40:58 +0200

    debug script --> temp benchmark

--- benchopt/utils/temp_benchmark.py
+++ benchopt/utils/temp_benchmark.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 from benchopt.benchmark import Benchmark
 
+
+# default params
 dummy_objective = """
 """
 
@@ -14,6 +16,46 @@ dummy_datasets = ("""
 
 dummy_solver = ("""
 """,)
+
+# example
+datasets = [
+    (
+        "from benchopt import BaseDataset\n"
+        "class Dataset(BaseDataset):\n"
+        "    name = 'dummy-dataset'\n"
+        "    def __init__(self, param=1.):\n"
+        "        self.param = param\n"
+        "    def get_data(self):\n"
+        "        return dict(a=1, b=1)\n"
+    ),
+]
+
+solvers = [
+    (
+        "from benchopt import BaseSolver, safe_import_context\n"
+        "with safe_import_context() as import_ctx_wrong_name:\n"
+        "   import numpy as np\n"
+        "class Solver(BaseSolver):\n"
+        "   name = 'test_import_ctx'\n"
+        "   def set_objective(self, a, b):\n"
+        "       self.a, self.b = a, b\n"
+        "   def run(self, n_iter):\n"
+        "       return -n_iter / 1000\n"
+    )
+]
+
+
+objective = (
+    "from benchopt import BaseObjective\n"
+    "import numpy as np\n"
+    "class Objective(BaseObjective):\n"
+    "   def set_data(self, a, b):\n"
+    "       self.a, self.b = a, b\n"
+    "   def get_objective(self):\n"
+    "       return dict(a=self.a, b=self.b)\n"
+    "   def compute(self, x):\n"
+    "       return np.exp(x)\n"
+)
 
 
 @contextlib.contextmanager
