67c212c5e15374d48839ba28d99ad0c9a77157e2
mathurinm <mathurin.massias@gmail.com>
2023-07-04 16:53:29 +0200

    read default values from dummy benchmark

--- benchopt/utils/temp_benchmark.py
+++ benchopt/utils/temp_benchmark.py
@@ -5,63 +5,31 @@ import contextlib
 from pathlib import Path
 
 from benchopt.benchmark import Benchmark
+from benchopt.tests import DUMMY_BENCHMARK_PATH
 
 
-# default params
-dummy_objective = """
-"""
+dummy_objective = (DUMMY_BENCHMARK_PATH / 'objective.py').read_text()
 
-dummy_datasets = ("""
-""",)
+dummy_solvers = [
+    (DUMMY_BENCHMARK_PATH / "solvers" / p).read_text()
+    for p in os.listdir(DUMMY_BENCHMARK_PATH / "solvers")
+    if p.endswith(".py")]
 
-dummy_solver = ("""
-""",)
-
-# example
-datasets = [
-    (
-        "from benchopt import BaseDataset\n"
-        "class Dataset(BaseDataset):\n"
-        "    name = 'dummy-dataset'\n"
-        "    def __init__(self, param=1.):\n"
-        "        self.param = param\n"
-        "    def get_data(self):\n"
-        "        return dict(a=1, b=1)\n"
-    ),
-]
-
-solvers = [
-    (
-        "from benchopt import BaseSolver, safe_import_context\n"
-        "with safe_import_context() as import_ctx_wrong_name:\n"
-        "   import numpy as np\n"
-        "class Solver(BaseSolver):\n"
-        "   name = 'test_import_ctx'\n"
-        "   def set_objective(self, a, b):\n"
-        "       self.a, self.b = a, b\n"
-        "   def run(self, n_iter):\n"
-        "       return -n_iter / 1000\n"
-    )
-]
-
-
-objective = (
-    "from benchopt import BaseObjective\n"
-    "import numpy as np\n"
-    "class Objective(BaseObjective):\n"
-    "   def set_data(self, a, b):\n"
-    "       self.a, self.b = a, b\n"
-    "   def get_objective(self):\n"
-    "       return dict(a=self.a, b=self.b)\n"
-    "   def compute(self, x):\n"
-    "       return np.exp(x)\n"
-)
+dummy_datasets = [
+    (DUMMY_BENCHMARK_PATH / "datasets" / p).read_text()
+    for p in os.listdir(DUMMY_BENCHMARK_PATH / "datasets")
+    if p.endswith(".py")]
 
 
 @contextlib.contextmanager
 def temp_benchmark(
-        objective=dummy_objective, datasets=dummy_datasets,
-        solvers=dummy_solver, config=None):
+        objective=dummy_objective, datasets=None,
+        solvers=None, config=None):
+    if solvers is None:
+        solvers = dummy_solvers
+    if datasets is None:
+        datasets = dummy_datasets
+
     with tempfile.TemporaryDirectory() as tempdir:
         temp_path = Path(tempdir)
         os.mkdir(temp_path / "solvers")
