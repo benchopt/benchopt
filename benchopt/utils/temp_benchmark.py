9301ffb822d3a49c99fcea9d6aa57e751e240687
mathurinm <mathurinm@users.noreply.github.com>
2023-07-05 10:49:00 +0200

    Review TM
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- benchopt/utils/temp_benchmark.py
+++ benchopt/utils/temp_benchmark.py
@@ -13,18 +13,23 @@ dummy_objective = (DUMMY_BENCHMARK_PATH / 'objective.py').read_text()
 dummy_solvers = [
     (DUMMY_BENCHMARK_PATH / "solvers" / p).read_text()
     for p in os.listdir(DUMMY_BENCHMARK_PATH / "solvers")
-    if p.endswith(".py")]
+    if p.endswith(".py")
+]
 
 dummy_datasets = [
     (DUMMY_BENCHMARK_PATH / "datasets" / p).read_text()
     for p in os.listdir(DUMMY_BENCHMARK_PATH / "datasets")
-    if p.endswith(".py")]
+    if p.endswith(".py")
+]
 
 
 @contextlib.contextmanager
 def temp_benchmark(
-        objective=dummy_objective, datasets=None,
-        solvers=None, config=None):
+        objective=None, datasets=None,
+        solvers=None, config=None
+):
+    if objective is None:
+        objective = dummy_objective
     if solvers is None:
         solvers = dummy_solvers
     if datasets is None:
@@ -32,16 +37,16 @@ def temp_benchmark(
 
     with tempfile.TemporaryDirectory() as tempdir:
         temp_path = Path(tempdir)
-        os.mkdir(temp_path / "solvers")
-        os.mkdir(temp_path / "datasets")
+        (temp_path / "solvers").mkdir()
+        (temp_path / "datasets").mkdir()
         with open(temp_path / "objective.py", "w") as f:
             f.write(objective)
         for idx, dataset in enumerate(datasets):
-            with open(temp_path / "solvers" / f"{idx}.py", "w") as f:
+            with open(temp_path / "solvers" / f"solver_{idx}.py", "w") as f:
                 f.write(dataset)
 
         for idx, solver in enumerate(solvers):
-            with open(temp_path / "datasets" / f"{idx}.py", "w") as f:
+            with open(temp_path / "datasets" / f"dataset_{idx}.py", "w") as f:
                 f.write(solver)
 
         if config is not None:
