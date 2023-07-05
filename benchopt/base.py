5e173de87128f4218afeec6d500c9e54a69f02fc
Pierre Ablin <pierreablin@gmail.com>
2023-07-05 16:35:11 +0200

    flake8

--- benchopt/base.py
+++ benchopt/base.py
@@ -233,10 +233,10 @@ class BaseSolver(ParametrizedNameMixin, DependenciesMixin, ABC):
     def warm_up(self):
         """User specified warm up step, called once before the runs.
         The time it takes to run this function is not taken into account.
-        The function `Solver.run_once` can be used here for solvers that 
+        The function `Solver.run_once` can be used here for solvers that
         require jit compilation."""
         ...
-    
+
     def _warm_up(self):
         if not getattr(self, 'warmup_done', True):
             self.warm_up()
