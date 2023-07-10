90efee813967b767919169a351d866e316e496b0
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-10 15:25:04 +0200

    fix: use matlab conventions.

--- benchopt/tests/test_benchmarks/dummy_benchmark/solvers/matlab_pgd.m
+++ benchopt/tests/test_benchmarks/dummy_benchmark/solvers/matlab_pgd.m
@@ -1,11 +1,10 @@
 function w = matlab_pgd(X, y, lambda, n_iter)
     L = norm(X, 'fro')^2;
-
     n_features = size(X, 2);
     w = zeros(n_features, 1);
     t_new = 1;
     for i = 1:n_iter
-        grad = X' * (X * w - y);
+        grad = X' * (X * w - y');
         w = w - grad / L;
         w = st(w, lambda / L);
     end
