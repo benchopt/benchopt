23ab3ef07eeefac8d92fc4c506c0c26913686949
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-10 11:00:56 +0200

    feat: add a matlab solver.

---
+++ benchopt/helpers/matlab.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+
+from benchopt.base import BaseSolver
+
+
+def assert_julia_installed():
+    import matlab
+
+@contextmanager()
+def matlab_engine(paths):
+    """Context manager to start a matlab engine and close it properly.
+
+    Parameters
+    ----------
+    paths : list of str or str
+        List of paths to add to the matlab path.
+
+    Yields
+    ------
+    eng : matlab.engine
+        The matlab engine.
+    """
+    import matlab.engine
+    eng = matlab.engine.start_matlab()
+    for path in paths:
+        eng.addpath(path)
+    yield eng
+    eng.quit()
+
+
+
+class MatlabSolver(BaseSolver):
+
+    requirements = ['pip:matlabengine']
