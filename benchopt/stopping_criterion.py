434a6e4962f3a6238ff7cfa73a38a3f699f4747c
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-07-12 12:45:00 +0200

    rm class

--- benchopt/stopping_criterion.py
+++ benchopt/stopping_criterion.py
@@ -466,11 +466,3 @@ class SingleRunCriterion(StoppingCriterion):
 
     def check_convergence(self, cost_curve):
         return True, 1
-
-
-class NoCriterion(StoppingCriterion):
-    """Run the solvers for a number of time fixed by max_iter and timeout.
-    """
-
-    def check_convergence(self, cost_curve):
-        return False, 0
