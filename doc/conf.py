0f7f71ac358cda80f67e3cf98c497882103d7d1e
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-07-13 13:18:57 +0200

    init new theme

--- doc/conf.py
+++ doc/conf.py
@@ -99,7 +99,7 @@ numpydoc_show_class_members = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
