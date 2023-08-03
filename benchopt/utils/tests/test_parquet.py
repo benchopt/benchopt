e886911d893c655c5482fb515ff048825b5da373
tommoral <thomas.moreau.2010@gmail.com>
2023-08-04 01:34:53 +0200

    TST improve the tests

--- benchopt/utils/tests/test_parquet.py
+++ benchopt/utils/tests/test_parquet.py
@@ -8,7 +8,11 @@ from benchopt.utils.parquet import update_metadata
 
 
 def test_parquet_metadata():
-    df = pd.DataFrame({'a': range(4), 'b': list('abcd')})
+    df = pd.DataFrame({
+        'a': range(4),
+        'b': list('abcd'),
+        'c': [.1, .2, .3, .4],
+    })
 
     with tempfile.NamedTemporaryFile("rb+", suffix=".pq") as f:
 
@@ -16,12 +20,20 @@ def test_parquet_metadata():
         metadata = {'test': 'info', 'plot_configs': [{}, {}, {}]}
         to_parquet(df, path, metadata)
 
+        # Make sure the data is readable
         df_new = pd.read_parquet(path)
         assert (df_new == df).all().all()
 
+        # Check that the metadata can be retrieved
         assert json.dumps(get_metadata(path)) == json.dumps(metadata)
 
         meta_update = {'test': 'ofni', 'a': 'b'}
         metadata.update(meta_update)
         update_metadata(path, meta_update)
+
+        # Make sure the data did not change
+        df_new = pd.read_parquet(path)
+        assert (df_new == df).all().all()
+
+        # Check that the metadata has been changed correctly
         assert json.dumps(get_metadata(path)) == json.dumps(metadata)
