12b01c0e59e31ddb50f46b878d5552d472a4524c
Melvine Nargeot <melvine.nargeot@gmail.com>
2023-11-10 09:31:24 +0100

    Add boxplot_chart to constants

--- benchopt/constants.py
+++ benchopt/constants.py
@@ -2,5 +2,6 @@ PLOT_KINDS = {
     'objective_curve': 'plot_objective_curve',
     'suboptimality_curve': 'plot_suboptimality_curve',
     'relative_suboptimality_curve': 'plot_relative_suboptimality_curve',
-    'bar_chart': 'plot_bar_chart'
+    'bar_chart': 'plot_bar_chart',
+    'boxplot_chart': 'plot_boxplot_chart'
 }
