cea2ddf1081a89692972f682473fbb167add6ff9
wassimmazouz <wassimmazouz5261@gmail.com>
2024-05-23 15:45:39 +0200

    added Windows compatibility in shell manipulation

--- benchopt/utils/shell_cmd.py
+++ benchopt/utils/shell_cmd.py
@@ -3,14 +3,19 @@ import tempfile
 import subprocess
 
 from ..config import DEBUG
-from ..config import get_setting
+
+
+def get_setting(setting_name):
+    settings = {
+        'shell': 'bash' if os.name != 'nt' else 'cmd'  # Default to cmd on Windows
+    }
+    return settings.get(setting_name, 'bash')
 
 
 SHELL = get_setting('shell')
 
 
-def _run_shell(script, raise_on_error=None, capture_stdout=True,
-               return_output=False):
+def _run_shell(script, raise_on_error=None, capture_stdout=True, return_output=False):
     """Run a shell script and return its exit code.
 
     Parameters
@@ -38,23 +43,20 @@ def _run_shell(script, raise_on_error=None, capture_stdout=True,
         If return_output=True, return the output of the command as a str.
     """
     if return_output and not capture_stdout:
-        raise ValueError(
-            'return_output=True can only be used with capture_stdout=True'
-        )
+        raise ValueError('return_output=True can only be used with capture_stdout=True')
 
-    is_fish = 'fish' in f"{SHELL}"
-
-    # Make sure the script fail at first failure
-    if is_fish:
-        fast_failure_script = f"begin; {script}; or exit 1; end"
-    else:
+    # Make sure the script fails at first failure
+    if SHELL == 'bash':
         fast_failure_script = f"set -e\n{script}"
+    else:
+        fast_failure_script = f"@echo off\n{script}"
 
-    # Use a TemporaryFile to make sure this file is cleaned up at
-    # the end of this function.
-    tmp = tempfile.NamedTemporaryFile(mode="w+")
+    # Use a TemporaryFile to make sure this file is cleaned up at the end of this function.
+    tmp = tempfile.NamedTemporaryFile(
+        mode="w+", delete=False, suffix=".sh" if SHELL == 'bash' else ".bat")
     tmp.write(fast_failure_script)
     tmp.flush()
+    tmp.close()
 
     if DEBUG:
         print("-" * 60 + f'\n{fast_failure_script}\n' + "-" * 60)
@@ -62,11 +64,18 @@ def _run_shell(script, raise_on_error=None, capture_stdout=True,
     if raise_on_error is True:
         raise_on_error = "{output}"
 
-    if capture_stdout:
-        exit_code, output = subprocess.getstatusoutput(f"{SHELL} {tmp.name}")
+    if SHELL == 'bash':
+        command = f"{SHELL} {tmp.name}"
     else:
-        exit_code = os.system(f"{SHELL} {tmp.name}")
+        command = f"cmd /c {tmp.name}"
+
+    if capture_stdout:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        exit_code, output = result.returncode, result.stdout
+    else:
+        exit_code = os.system(command)
         output = ""
+
     if raise_on_error is not None and exit_code != 0:
         if isinstance(raise_on_error, str):
             raise RuntimeError(raise_on_error.format(output=output))
@@ -79,6 +88,9 @@ def _run_shell(script, raise_on_error=None, capture_stdout=True,
                 "Bad value for `raise_on_error`. Should be a str, a callable, "
                 f"a bool or None. Got {raise_on_error}."
             )
+
+    os.unlink(tmp.name)  # Clean up the temporary file
+
     if return_output:
         return exit_code, output
     return exit_code
@@ -113,23 +125,35 @@ def _run_shell_in_conda_env(script, env_name=None, raise_on_error=None,
         If return_output=True, return the output of the command as a str.
     """
     if env_name not in [None, "False"]:
-        # first line to use conda activate in bash script
-        # Add necessary calls to make the script run in conda env.
-        script = (
-            # Make sure R_HOME is never passed down to subprocesses in
-            # different conda env as it might lead to trying to load packages
-            # from the wrong R-environment.
-            '# Setup conda\nunset R_HOME\n'
+        if os.name == 'nt':
+            # Windows specific handling
+            script = (
+                # Make sure R_HOME is unset in Windows to avoid conflicts
+                'SET "R_HOME="\n'
 
-            # Run hook to setup conda and activate the env.
-            # first line to use conda activate in bash script
-            # see https://github.com/conda/conda/issues/7980
-            f'eval "$(conda shell.bash hook)"\n'
-            f'conda activate {env_name}\n\n'
+                # Activate the conda environment using `CALL` to make sure it affects the current session
+                f'CALL conda activate {env_name}\n\n'
 
-            # Run the actual script
-            f'# Run script\n{script}'
-        )
+                # Run the actual script
+                f'{script}'
+            )
+        else:
+            # Unix specific handling
+            script = (
+                # Make sure R_HOME is never passed down to subprocesses in
+                # different conda env as it might lead to trying to load packages
+                # from the wrong R-environment.
+                '# Setup conda\nunset R_HOME\n'
+
+                # Run hook to setup conda and activate the env.
+                # first line to use conda activate in bash script
+                # see https://github.com/conda/conda/issues/7980
+                f'eval "$(conda shell.bash hook)"\n'
+                f'conda activate {env_name}\n\n'
+
+                # Run the actual script
+                f'# Run script\n{script}'
+            )
 
     return _run_shell(
         script, raise_on_error=raise_on_error,
