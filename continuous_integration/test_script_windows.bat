37546081b78df8b4d6d81b4236febfb9e561c746
wassimmazouz <wassimmazouz5261@gmail.com>
2024-06-04 15:53:56 +0200

    update batch file for windows test

--- continuous_integration/test_script_windows.bat
+++ continuous_integration/test_script_windows.bat
@@ -1,4 +1,36 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
 conda info
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 pip freeze
+if %errorlevel% neq 0 exit /b %errorlevel%
 
+set TEST_CMD=python -m pytest -vs --durations=20 --junitxml=%JUNIT_XML%
+set TEST_CMD=%TEST_CMD% --test-env %CONDA_ENV%
 
+REM Un-comment when debugging the CI
+REM set TEST_CMD=%TEST_CMD% --skip-install
+
+if "%COVERAGE%"=="true" (
+    set COVERAGE_PROCESS_START=.coveragerc
+    set TEST_CMD=%TEST_CMD% --cov=benchopt --cov-append
+    python continuous_integration\install_coverage_subprocess_pth.py
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
+
+@echo on
+%TEST_CMD%
+if %errorlevel% neq 0 exit /b %errorlevel%
+%TEST_CMD% --skip-install --cov-append
+if %errorlevel% neq 0 exit /b %errorlevel%
+@echo off
+
+if "%COVERAGE%"=="true" (
+    coverage xml -i
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
+
+exit /b 0
