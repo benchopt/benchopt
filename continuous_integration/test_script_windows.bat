16204ac33eb08433c1b181eb5e10cc131dd55417
wassimmazouz <wassimmazouz5261@gmail.com>
2024-06-07 15:01:52 +0200

    percentage variable manip

--- continuous_integration/test_script_windows.bat
+++ continuous_integration/test_script_windows.bat
@@ -8,11 +8,11 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 pip freeze
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call conda activate CONDA_ENV
+call conda activate base
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-set TEST_CMD=python -m pytest -vs --durations=20 --junitxml=JUNIT_XML
-set TEST_CMD=python -m pytest -vs --durations=20 --junitxml=JUNIT_XML --test-env CONDA_ENV
+set TEST_CMD=python -m pytest -vs --durations=20 --junitxml="%JUNIT_XML%"
+set TEST_CMD=python -m pytest -vs --durations=20 --junitxml="%JUNIT_XML%" --test-env "%CONDA_ENV%"
 
 REM Un-comment when debugging the CI
 REM set TEST_CMD=%TEST_CMD% --skip-install
