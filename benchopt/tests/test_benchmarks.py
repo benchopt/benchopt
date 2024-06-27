167b692edc915e779a6e282847b19192cdb11c42
wassimmazouz <wassimmazouz5261@gmail.com>
2024-06-27 16:56:23 +0200

    skipping less tests

--- benchopt/tests/test_benchmarks.py
+++ benchopt/tests/test_benchmarks.py
@@ -113,10 +113,21 @@ def test_solver_install_api(benchmark, solver_class):
         assert hasattr(solver_class, 'install_script')
 
 
-@pytest.mark.skipif(sys.platform == 'win32',
-                    reason="Skipping test on Windows because of R-PGD")
 @pytest.mark.requires_install
 def test_solver_install(test_env_name, benchmark, solver_class, check_test):
+    # Skipping the specific R-PGD solver for Windows
+    if sys.platform == 'win32':
+        if hasattr(benchmark, 'name'):
+            benchmark_name = benchmark.name
+        else:
+            benchmark_name = str(benchmark)
+        solver_class_name = solver_class.__module__ + "."
+        solver_class_name += solver_class.__name__
+        if (
+            benchmark_name == "dummy_benchmark" and solver_class_name ==
+            "benchopt_benchmarks.dummy_benchmark.solvers.r_pgd.Solver"
+        ):
+            pytest.skip("Skipping the specific R-PGD solver for Windows")
 
     if check_test is not None:
         check_test(solver_class)
