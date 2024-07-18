6c71e8b0ecf11f64258472068fb10c0929a469a3
wassimmazouz <wassimmazouz5261@gmail.com>
2024-07-18 10:28:15 +0200

    DBG rpgd in instead of ==

--- benchopt/tests/dummy_benchmark/test_config.py
+++ benchopt/tests/dummy_benchmark/test_config.py
@@ -19,5 +19,8 @@ def check_test_solver_install(solver_class):
 
     # R-PGD solver needs R to be added manually to path
     # https://stackoverflow.com/questions/63863449/oserror-cannot-load-library-c-program-files-r-r-4-0-2-bin-x64-r-dll-error-0
-    if solver_class.name.lower() == 'r_pgd' and sys.platform == 'win32':
+    if (
+        ('R-PGD' in solver_class.name.lower()
+         or 'r_pgd' in solver_class.name.lower()) and sys.platform == 'win32'
+    ):
         pytest.xfail('R-PGD is not easy to install on Windows.')
