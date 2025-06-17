213e3ee760d18c2216fd07b98800a8bdec94148d
Johan Larsson <jolars@posteo.com>
2025-06-17 15:56:18 +0200

    FIX move `run_statistics.extend` inside `try` block
    `curve` may otherwise be unbound. Closes #808.

--- benchopt/runner.py
+++ benchopt/runner.py
@@ -277,12 +277,12 @@ def run_one_solver(benchmark, dataset, objective, solver, n_repetitions,
             curve, status = run_one_to_cvg_cached(
                 **args_run_one_to_cvg
             )
+            run_statistics.extend(curve)
         except RuntimeError as e:
             status = e.args[0]
         if status in ['diverged', 'error', 'interrupted', 'not run yet']:
             run_statistics = []
             break
-        run_statistics.extend(curve)
         states.append(status)
 
     else:
