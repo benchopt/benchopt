288fe9b0f8929ad53c6bdc22d0f20d55e0faa01e
tommoral <thomas.moreau.2010@gmail.com>
2025-07-22 19:27:59 +0200

    MTN skip julia install

--- benchopt/tests/dummy_benchmark/test_config.py
+++ benchopt/tests/dummy_benchmark/test_config.py
@@ -8,11 +8,14 @@ def check_test_solver_install(solver_class):
     if solver_class.name.lower() == 'cyanure' and sys.platform == 'darwin':
         pytest.xfail('Cyanure is not easy to install on macos.')
 
-    # Skip test_solver_install for julia in OSX as there is a version
-    # conflict with conda packages for R
-    # See issue #64
-    if 'julia' in solver_class.name.lower() and sys.platform == 'darwin':
-        pytest.xfail('Julia causes segfault on OSX for now.')
+    if 'julia' in solver_class.name.lower():
+        # Skip test_solver_install for julia in OSX as there is a version
+        # conflict with conda packages for R
+        # See issue #64
+        if sys.platform == 'darwin':
+            pytest.xfail('Julia causes segfault on OSX for now.')
+        else:
+            pytest.xfail("Julia is broken on other platforms too")
 
     if solver_class.name.lower() in ["solver-test"]:
         pytest.skip('Test solver that cannot be installed, skipping.')
