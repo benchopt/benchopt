3b0e905d4c0d2628bed111ffd5665d345ba6fee9
Hippolyte Verninas <etyl.sani@gmail.com>
2026-01-05 16:18:49 +0100

    ENH: plot all objective values

--- examples/minimal_benchmark/plots/custom_table.py
+++ examples/minimal_benchmark/plots/custom_table.py
@@ -8,26 +8,44 @@ class Plot(BasePlot):
 
     def plot(self, df):
         plots = []
+
+        # Get numeric columns starting with 'objective_'
+        df_filtered = df.select_dtypes(include=['number']).columns
+        objective_cols = [
+            col for col in df_filtered if col.startswith('objective_')
+        ]
+
         for solver in df['solver_name'].unique():
+            solver_res = [solver]
             solver_df = df[df['solver_name'] == solver]
-            # Get the final objective value and time for each solver
-            final_objective = float(
-                solver_df.select_dtypes(include=['number'])
-                .groupby('stop_val')
-                .median()["objective_value"]
-                .values[-1]
-            )
+            for col in objective_cols:
+                if col not in solver_df.columns:
+                    continue
+                median_val = float(
+                    solver_df.select_dtypes(include=['number'])
+                    .groupby('stop_val')
+                    .median()[col]
+                    .values[-1]
+                )
+                solver_res.append(median_val)
+
             final_time = float(
                 solver_df.select_dtypes(include=['number'])
                 .groupby('stop_val')
-                .median()
-                .index[-1]
+                .median()['time']
+                .values[-1]
             )
-            plots.append([solver, final_objective, final_time])
+            solver_res.append(final_time)
+            plots.append(solver_res)
         return plots
 
     def get_metadata(self, df):
+        df_filtered = df.select_dtypes(include=['number']).columns
+        objective_cols = [
+            col for col in df_filtered if col.startswith('objective_')
+        ]
+        columns = ["solver"] + objective_cols + ["time (s)"]
         return {
             "title": "Comparison of solvers",
-            "columns": ["solver", "value", "time"],
+            "columns": columns,
         }
