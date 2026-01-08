81f01bf4ffb08268ca26905877678309069ad4f5
Hippolyte Verninas <55251414+Etyl@users.noreply.github.com>
2026-01-08 12:05:35 +0100

    Apply suggestions from code review
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- benchopt/plotting/default_plots.py
+++ benchopt/plotting/default_plots.py
@@ -215,24 +215,18 @@ class TablePlot(BasePlot):
             'dataset_name == @dataset and objective_name == @objective'
         )
         # Get numeric columns starting with 'objective_'
-        df_filtered = df.select_dtypes(include=['number']).columns
         objective_cols = [
-            col for col in df_filtered if col.startswith('objective_')
+            col for col in df.select_dtypes(include=['number']).columns
+            if col.startswith('objective_')
         ]
 
-        for solver in df['solver_name'].unique():
+        for solver, solver_df in df.groupby('solver_name'):
             solver_res = [solver]
-            solver_df = df[df['solver_name'] == solver]
-            for col in objective_cols:
-                if col not in solver_df.columns:
-                    continue
-                median_val = float(
-                    solver_df.select_dtypes(include=['number'])
-                    .groupby('stop_val')
-                    .median()[col]
-                    .values[-1]
-                )
-                solver_res.append(median_val)
+           median_vals = (
+               solver_df.groupby('stop_val')
+               .median(numeric_only=True).iloc[-1]
+            )[objective_list + ['time']].to_list()
+            solver_res.extend(median_vals)
 
             final_time = float(
                 solver_df.select_dtypes(include=['number'])
