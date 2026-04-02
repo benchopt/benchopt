4237afb84956636c85b5e254a145477ec18bebf2
tommoral <thomas.moreau.2010@gmail.com>
2026-04-02 23:41:10 +0200

    CLN make func private

--- benchopt/plotting/__init__.py
+++ benchopt/plotting/__init__.py
@@ -21,24 +21,24 @@ ALL_PLOT_OPTIONS = [
 ]
 
 
-def sanitize_config(config):
+def _sanitize_config(config):
     """Flatten a pytree into a list."""
     if isinstance(config, (list, tuple)):
         # Avoid duplicates while preserving order
         seen = set()
         return [
-            sanitize_config(item) for item in config
+            _sanitize_config(item) for item in config
             if item not in seen and not seen.add(item)
         ]
     elif isinstance(config, dict):
         return {
-            k: sanitize_config(v) for k, v in config.items()
+            k: _sanitize_config(v) for k, v in config.items()
         }
     else:
         return BACKWARD_COMPAT_PLOTS.get(config, config)
 
 
-def check_view(name, view, plots):
+def _check_view(name, view, plots):
     all_options = ALL_PLOT_OPTIONS
     all_kinds = [plot._get_name() for plot in plots]
 
@@ -98,7 +98,7 @@ def plot_benchmark(fname, benchmark, kinds=None, display=True, plotly=False,
     """
     plot_config = get_metadata(fname)
     plot_config = benchmark.get_plot_config(default_config=plot_config)
-    plot_config = sanitize_config(plot_config)
+    plot_config = _sanitize_config(plot_config)
     update_metadata(fname, plot_config)
 
     if kinds is not None and len(kinds) > 0:
@@ -121,7 +121,7 @@ def plot_benchmark(fname, benchmark, kinds=None, display=True, plotly=False,
             )
 
     for name, view in plot_config.get("plot_configs", {}).items():
-        check_view(name, view, plots)
+        _check_view(name, view, plots)
 
     if html:
         plot_benchmark_html(fname, benchmark, plot_config, display)
